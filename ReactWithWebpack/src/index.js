75aef62c684b7b4544b8cc47ef7a3a039c79b8b4
informatropico <informatropico@gmail.com>
2023-07-11 11:06:16 +0200

    React App with Webpack

---
+++ ReactWithWebpack/src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <h1>Rick and Morty API test</h1>
+    );
+}
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
---
+++ ReactWithWebpack/webpack.config.js
@@ -0,0 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html',
+        })
+    ]
+};
